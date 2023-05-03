4e0be0d9c7be4fb9786ebc0c89a4ffb3fd410533
Rohit Pant <120168154+rohitpant788@users.noreply.github.com>
2023-05-03 21:15:33 +0530

    Update delete.js

--- firstname/src/component/delete.js
+++ firstname/src/component/delete.js
@@ -1,23 +1,19 @@
-(
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <div style={{ marginLeft: '20px' }}>Todo Tasks</div>
-        <div>
-          <button style={{ marginLeft: '20px' }}>Button 1</button>
-          <button style={{ marginLeft: '20px' }}>Button 2</button>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ marginRight: '20px' }}>
-              <DataTable.DownloadCsv />
-            </div>
-            <div style={{ marginRight: '20px' }}>
-              <DataTable.Print />
-            </div>
-            <div style={{ marginRight: '20px' }}>
-              <DataTable.ViewColumns />
-            </div>
-            <div style={{ marginRight: '20px' }}>
-              <button>Refresh</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    backgroundColor: theme.palette.background.paper, // use the same background color as the DataGrid
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    '& .MuiTypography-root': {
+      fontWeight: 500,
+      textTransform: 'uppercase',
+      letterSpacing: 1.2,
+      fontSize: '1.2rem',
+      color: theme.palette.text.secondary,
+      marginTop: theme.spacing(1),
+    },
+  },
+}));
